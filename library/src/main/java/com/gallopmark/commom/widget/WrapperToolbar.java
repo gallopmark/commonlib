5b13eddf84f4fb904254fd0467d4db840b99aed9
tthwy <i23xiaoyan@qq.com>
2019-06-20 11:55:13 +0800

    新增自定义Toolbar title居中显示

--- library/src/main/java/com/gallopmark/commom/widget/WrapperToolbar.java
+++ library/src/main/java/com/gallopmark/commom/widget/WrapperToolbar.java
@@ -16,7 +16,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.gallopmark.commom.R;
+import android.support.v7.appcompat.R;
 
 import java.lang.reflect.Field;
 
